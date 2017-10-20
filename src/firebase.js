b4621f80a124cc91eda46c4b8dfe2d850a75573a
Carol Enache <enache.carol@gmail.com>
2017-10-20 12:04:37 +0100

    link the app to Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyAYw0nvl7EPJdtYK9H5gMHDKe_qm4UL-W8",
+  authDomain: "spirit-animal-look-book-136a0.firebaseapp.com",
+  databaseURL: "https://spirit-animal-look-book-136a0.firebaseio.com",
+  projectId: "spirit-animal-look-book-136a0",
+  storageBucket: "spirit-animal-look-book-136a0.appspot.com",
+  messagingSenderId: "889458114785"
 };
 
 firebase.initializeApp(config);
