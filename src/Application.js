d8d23c84951613c76f78f08b6ef309765fc968a3
Carol Enache <enache.carol@gmail.com>
2017-10-20 11:57:05 +0100

    when user logs in check if it is in the Database, if not save it in the database, also set an event listener fro all teh user an have sa setState ready for action.

--- src/Application.js
+++ src/Application.js
@@ -18,6 +18,25 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    auth.onAuthStateChanged((user)=>{
+      if (user) {
+        this.setState({ user });
+        this.usersRef = database.ref('/users');
+        this.userRef = this.userRef.child(user.uid);
+        this.userRef.once('value').them((snapshot) => {
+          if(snapshot.val()) retrun;
+          const userData = pick(user, ['displayName', 'photoURL', 'email'])
+          this.userRef.set(userData);
+        });
+
+        this.userRef.on('value', (snapshot)=> {
+          this.setState({users: snapshot.val() });
+        });
+      }
+    });
+  }
+
   render() {
     const { user, users } = this.state;
 
